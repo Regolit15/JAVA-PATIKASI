9a571988d7102938946fd373b55f284450ac0450
Berat G <86969477+Regolit15@users.noreply.github.com>
2022-06-10 14:53:52 +0300

    Uslu Sayi Hesaplama

--- usluSayiHesaplayan/src/usluSayi.java
+++ usluSayiHesaplayan/src/usluSayi.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class usluSayi {
     public static void main(String[] args) {
         int a, b, total = 1;
-        System.out.println("Us Hesaplama Programina Hos Geldiniz!");
+        System.out.println("Uslu Sayi Hesaplama Programina Hos Geldiniz!");
         Scanner scan = new Scanner(System.in);
         System.out.print("Taban Sayisini Giriniz : ");
         a = scan.nextInt();
