a6ceec57f19b0e69f8b210dcacb7dc1d88366ddf
Berat G <86969477+Regolit15@users.noreply.github.com>
2022-06-16 00:05:55 +0300

    Switch Case Atm

--- atmProject/src/homeWork.java
+++ atmProject/src/homeWork.java
@@ -8,46 +8,48 @@ public class homeWork {
         int balance = 3000;
         int select;
 
-
+        System.out.print("Kullanici Adiniz :");
+        userName = input.nextLine();
+        System.out.print("Sifreniz :");
+        passWord = input.nextLine();
         while (right > 0) {
-            System.out.print("Kullanici Adiniz :");
-            userName = input.nextLine();
-            System.out.print("Sifreniz :");
-            passWord = input.nextLine();
-
             if (userName.equals("Regolit15") && passWord.equals("pati123")) {
                 System.out.println("Merhaba, Java Bankasina Hos Geldiniz!");
-                System.out.println("""
-                        Lutfen Yapacaginiz Islemi Rakamlar ile Belirtiniz :
-                        1-Para Yatirma
-                        2-Para Cekme
-                        3-Bakiye Sorgulama
-                        4-Cikis Yap
-                        """);
+                System.out.println("Lutfen Yapacaginiz Islemi Rakamlar ile Belirtiniz :\n" +
+                        "1-Para Yatirma\n" +
+                        "2-Para Cekme\n" +
+                        "3-Bakiye Sorgulama\n" +
+                        "4-Cikis Yap\n");
+
                 System.out.print("Seciminiz :");
                 select = input.nextInt();
-
                 switch (select) {
-                    case 1 -> {
+                    case 1:
                         System.out.print("Yatirma Istediginiz Tutari Giriniz :");
                         int price = input.nextInt();
                         balance += price;
-                    }
-                    case 2 -> {
+                        break;
+                    case 2:
                         System.out.print("Cekmek istediginiz Tutari Giriniz :");
                         int pric = input.nextInt();
+
                         if (pric > balance) {
                             System.out.println("Hesabiniz Yeterli Bakiye Bulunmamaktadir!");
                         } else {
                             balance -= pric;
                         }
-                    }
-                    case 3 -> System.out.print("Hesabinizin Bakiyesi :" + balance);
-                    case 4 -> System.out.println("Gule Gule");
-                    default -> System.out.print("Secim Hatasi Tekrar Deneyiniz!");
+                        break;
+                    case 3:
+                        System.out.print("Hesabinizin Bakiyesi :" + balance);
+                        break;
+                    case 4:
+                        System.out.println("Gule Gule");
+                        break;
+
+                    default:
+                        System.out.print("Seciminiz Hatali Tekrar Deneyiniz!");
 
                 }
-
 
             } else {
                 right--;
