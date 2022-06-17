89d8b4bef2f3fd0fd758a4777b495adcf2512c62
Berat G <86969477+Regolit15@users.noreply.github.com>
2022-06-17 03:47:44 +0300

    Max and Min Degerleri bulan

--- maxMinnumber/src/homeWork.java
+++ maxMinnumber/src/homeWork.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
+
 public class homeWork {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Girmek Istediginiz Sayi Miktarini Giriniz : ");
-        int say=scan.nextInt();
-        int max=1,min=1;
-        for(int i =1;i<=say;i++){
-            System.out.print(i+ ".Sayi");
-            int num=scan.nextInt();
+        int say = scan.nextInt();
+        int max = 1, min = 1;
+        for (int i = 1; i <= say; i++) {
+            System.out.print(i + ".Sayi");
+            int num = scan.nextInt();
 
-            if(i==1){
-                max=num;
-                min=num;
+            if (i == 1) {
+                max = num;
+                min = num;
             }
-            if(num>max){
-                max=num;
+            if (num > max) {
+                max = num;
             }
             if (num < min) {
-                min=num;
+                min = num;
             }
         }
-        System.out.println("Max Olan Sayi : "+max);
-        System.out.print("Min Olan Sayi : "+min);
+        System.out.println("Max Olan Sayi : " + max);
+        System.out.print("Min Olan Sayi : " + min);
     }
 }
