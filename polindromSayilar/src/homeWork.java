81c9981aff5ab66da415ed3490f64f9d6a03901b
Berat G <86969477+Regolit15@users.noreply.github.com>
2022-06-23 22:22:40 +0300

    Fibonacci Series

---
+++ polindromSayilar/src/homeWork.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class homeWork {
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }if(number==reverseNumber){
+            System.out.print("Sayiniz Polindrom Sayidir");
+        }else{
+            System.out.print("Polindrom Degildir.");
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        isPalindrom(505);
+    }
+}
