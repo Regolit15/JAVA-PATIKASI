12a13c4901dfbdf4c772c0b8a066d8f42373003e
Berat G <86969477+Regolit15@users.noreply.github.com>
2022-06-29 14:07:43 +0300

    Recursive ile Us alma Islemi

---
+++ recursiveileAsalsayi/src/homew.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class homew {
+    static boolean isPrime(int sayi,int i){
+
+        if(sayi<=2){
+            return (sayi==2) ? true : false;
+        }
+        if(sayi%i==0){
+            return false;
+        }
+        if(i*i>sayi){
+            return true;
+
+        }
+        return isPrime(sayi,i+1);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Programa Hos Geldiniz!");
+        System.out.print("Lutfen Sayi Giriniz : ");
+        int sayi=scan.nextInt();
+        if(isPrime(sayi,2)){
+            System.out.println(sayi+ " Sayisi Asal Sayidir");
+        }else{
+            System.out.println(sayi+ " Sayisi Asal Sayi Degildir");
+        }
+
+    }
+}
+
