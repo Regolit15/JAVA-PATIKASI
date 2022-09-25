3cb7933ffaf71a131e00245196b47df1449df96f
Regolit15 <beratoguzhangoktas@gmail.com>
2022-09-25 17:52:57 +0300

    Home work

--- sortingProgram/src/hwork.java
+++ sortingProgram/src/hwork.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 public class hwork {
     public static void main(String[] args) {
         System.out.println(" Welcome To Our Sorting Program");
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Please To Enter Array Size (only Number):");
-        int n=scan.nextInt();
-        int[] list= new int[n];
+        int n = scan.nextInt();
+        int[] list = new int[n];
         System.out.println("Enter To Elements Of The Array :");
-        for(int i =0; i<n;i++){
-            System.out.print((i+1)+". Number: ");
-            list[i]=scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + ". Number: ");
+            list[i] = scan.nextInt();
         }
         System.out.print("Smallest To Largest Sorting Of Your Array:");
         Arrays.sort(list);
